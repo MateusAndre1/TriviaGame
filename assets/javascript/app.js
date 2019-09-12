3f0b71d0189b90c51f83e86b21e14cf8a82df844
Your Name <you@example.com>
2019-09-11 21:50:02 -0400

    pseudocode is complete!

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Andre Pseudocode! Clap!
+// Create and array of questions
+// Have a start button begin a quiz from those questions
+// Have a timer set and go through the questions 1 at a time
+// Have the questions and choices shown
+// Display if the answer was correct or wrong with a gif
+// Show how many questions in total with how many is left
+// Show how many correct out of the total
+// Have a restart button after questions are complete
+
