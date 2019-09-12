e3712ec631ce000f1458cfa1b5674c284cf556fc
Your Name <you@example.com>
2019-09-11 21:41:13 -0400

    questions have been added to a seperate js file

---
+++ assets/javascript/questions.js
@@ -0,0 +1,47 @@
+const quizQuestions = [
+    {
+        question: "jQuery is a?",
+        choices: ["JavaScript Libray", "JavaScript Language", "JavaScript Method", "None of the Above"],
+        correctAnswer: "JavaScript Library"
+    },
+    {
+        question: "jQuery uses CSS selectors to select elements?",
+        choices: ["False", "True", "Maybe"],
+        correctAnswer: "False"
+    },
+    {
+        question: "Which sign does jQuery use as a shortcut for jQuery?",
+        choices: ["The % Sign", "The ? Sign", "The $ Sign", "All of the Above"],
+        correctAnswer: "The $ Sign"
+    },
+    {
+        question: "With jQuery, look at the following selector: $('div'). What does it select?",
+        choices: ["All div elements", "The first div elemenet", "The last div element", "Every other div"],
+        correctAnswer: "All div elements"
+    },
+    {
+        question: "Is jQuery a library for client scripting or server scripting?",
+        choices: ["Server scripting", "Client scripting", "I don't understand the question"],
+        correctAnswer: "Server scripting"
+    },
+    {
+        question: "What is the correct jQuery code to set the background color of all p elements to red?",
+        choices: ["$('p').layout('background-color','red');", "$('p').manipulate('background-color','red');", "$('p').css('background-color','red')", "$('p').style('background-color','red')"],
+        correctAnswer: "$('p').css('background-color','red')"
+    },
+    {
+        question: "Which jQuery method is used to hide selected elements?",
+        choices: ["Hidden()", "Hide()", "Visible(false)", "Display(HideME)"],
+        correctAnswer: "Hide()"
+    },
+    {
+        question: "Which jQuery method is used to switch between adding/removing one or more classes (for CSS) from selected elements?",
+        choices: ["toggleClass()", "Switch()", "AltClass()", "SwitchClass()"],
+        correctAnswer: "SwitchClass()"
+    },
+    {
+        question: " jQuery's main focus is..",
+        choices: ["AJAX", "DOM Manipulation", "Animations", "All of the Above"],
+        correctAnswer: "All of the Above"
+    }
+];
